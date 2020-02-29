e0dbd9cc15751bb045f37070971b5bf894a46344
André Santos <andreppstos@gmail.com>
2020-02-29 00:15:14 -0300

    commit com procfile

--- app/config.py
+++ app/config.py
@@ -14,4 +14,4 @@ class Config():
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', True)
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
