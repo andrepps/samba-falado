46845495f2f9d2a7f1274ec35c079654eaa7332a
André Santos <andreppstos@gmail.com>
2020-03-01 18:19:24 -0300

    teste

--- app/config.py
+++ app/config.py
@@ -5,7 +5,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
 
 class Config():
-
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.googlemail.com')
