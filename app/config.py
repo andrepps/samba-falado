2467104f8377951e8f7a070bd56c5fe1b0754beb
André Santos <andreppstos@gmail.com>
2020-03-03 04:33:46 -0300

    1.0.0

--- app/config.py
+++ app/config.py
@@ -5,7 +5,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
 
 class Config():
-    APPLICATION_ROOT = '/sambafalado'
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.googlemail.com')
@@ -13,4 +12,4 @@ class Config():
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', True)
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'teste.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
